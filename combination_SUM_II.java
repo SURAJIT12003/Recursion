b270c0a5008c20884678cdef3eb9d21b9d32793b
SURAJIT12003 <128603388+SURAJIT12003@users.noreply.github.com>
2023-04-02 02:32:08 +0530

    Update combination_SUM_II.java

--- combination_SUM_II.java
+++ combination_SUM_II.java
@@ -12,17 +12,14 @@ import java.util.*;
 
 public class combination_SUM_II {
 
-    public static void combi_sum_II(int i,int target,int arr[],ArrayList<Integer> as,int n ){
-        
+    public static void combi_sum_II(int i,int target,int arr[],ArrayList<Integer> as,int n ,ArrayList<ArrayList<Integer>> ans ){
             if(target==0){
                 ArrayList<Integer> a = new ArrayList<>();
                 a.addAll(as);
-                System.out.println(a); return ;
+                ans.add(a);
+                return ;
             }
 
-           
-      
-       
             for(int in=i;in<=n;in++){
                
                 if(in>i && arr[in]==arr[in-1]){
@@ -32,16 +29,10 @@ public class combination_SUM_II {
                     break;
                 }
                 as.add(arr[in]);
-                combi_sum_II(i+1, target-arr[in], arr, as, n);
+                combi_sum_II(i+1, target-arr[in], arr, as, n,ans);
                 as.remove(as.size()-1);
 
             }
-           
-      
-
-        // i=i+1;
-        // combi_sum_II(i, target, arr, as, n);
-
     }
     public static void main(String[] args) {
         int arr[]=new int [5];
@@ -50,9 +41,14 @@ public class combination_SUM_II {
         arr[2]=1;
         arr[3]=2;
         arr[4]=2;
+     
         int n = arr.length;
-        int k = 4;
+     
+        int target = 4;
+     
+        Arrays.sort(arr);
+     
         ArrayList<Integer> a = new ArrayList<>();
-        combi_sum_II(0, k, arr, a, n-1);
+        combi_sum_II(0, target, arr, a, n-1);
     }
 }
