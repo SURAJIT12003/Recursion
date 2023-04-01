773029dba16d5d9d1a54e1ae570014d75b04e43e
SURAJIT12003 <128603388+SURAJIT12003@users.noreply.github.com>
2023-04-01 12:56:02 +0530

    Update String_Sub_Seq_Recursion.java

---
+++ String_Sub_Seq_Recursion.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+//PRINT ALL SUB-SEQUENCE IN A STRING USING RECUSION  -- (QUESSTION LINK - https://practice.geeksforgeeks.org/problems/power-set4302/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=power-set)
+
+//VERY IMPORTANT 
+public class String_Sub_Seq_Recursion {
+
+    // SUBSEQUENCE ADD IN ARRAYLIST 
+    public static ArrayList<String> subSeq(String str){
+
+        ArrayList<String> ans  = new ArrayList<>();
+        if(str.length()==0){
+          ans.add(" ");
+          return ans;
+        }
+        
+        char curr  = str.charAt(0);
+        ArrayList<String> smallAns = subSeq(str.substring(1));
+    
+        for(String ss:smallAns){
+          ans.add(ss);
+        
+          ans.add(curr+ss);
+        }
+    
+        return ans;
+    
+      }
+
+     //SUB-SEQUENCE PRINT  
+      public static void print_subSet(String str,String ans){
+        if(str.length()==0){
+            System.out.println(ans);
+            return ;
+        }
+        char curr = str.charAt(0);
+        String remain = str.substring(1);
+        print_subSet(remain,ans+curr);
+        print_subSet(remain, ans);
+  
+    }
+    
+    public static void main(String[] args) {
+         String sol="ABC";
+         ArrayList<String> arr = subSeq(sol);
+        // System.out.println(arr);
+        print_subSet(sol, "");
+    }
+}
