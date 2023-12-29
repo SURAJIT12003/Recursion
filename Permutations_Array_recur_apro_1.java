892b056743bcce67587f4a79516ddbbbe4879080
Surajit Dey <128603388+SURAJIT12003@users.noreply.github.com>
2023-12-29 12:38:15 +0530

    Update Permutations_Array_recur_apro_1.java

--- Permutations_Array_recur_apro_1.java
+++ Permutations_Array_recur_apro_1.java
@@ -35,6 +35,39 @@
                 }
             }
     }
+
+  // ****-- Same aproach easy to use code ----****
+  
+   // public void recur(int index,String S,StringBuffer  str ,TreeSet<String>ans,HashSet<Integer> map){
+   //      if(index>=S.length()){
+            
+   //          ans.add(str.toString());
+   //          return;
+   //      }
+        
+   //      for(int i=0;i<S.length();i++){
+   //          if(map.contains(i)==false ){
+   //              str.append(S.charAt(i));
+   //              map.add(i);
+   //              recur(index+1,S,str,ans,map);
+   //              str.deleteCharAt(str.length() - 1); 
+   //              map.remove(i);
+   //          }
+   //      }    
+   //  }
+  
+   //  public List<String> find_permutation(String S) {
+   //     List<String>ans = new ArrayList<>();
+   //     HashSet<Integer> map =  new HashSet<>();
+   //     TreeSet<String> dup =   new TreeSet<>();
+   //     recur(0,S,new StringBuffer(),dup,map);
+   //     for(String v:dup){
+   //         ans.add(v);
+   //     }
+   //     return ans;
+       
+   //  }
+  
         public static void main(String[] args) {
 
             int arr[]=new int [3];
